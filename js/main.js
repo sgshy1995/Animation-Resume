97d6c82fb6c299a6d71134c5836bafb010668c89
Singlesaul <singlesaulwork@gmail.com>
2019-12-03 11:15:33 +0800

    功能完善

--- js/main.js
+++ js/main.js
@@ -14,14 +14,17 @@ var result = `
 }
 
 html {
-    background: rgb(222,222,222);
-    font-size: 16px;
+    background: #e8e8ef;
+    font-size: 14px;
 }
+
+/* 调整一下字体 */
 
 #code {
     font-family: Helvetica, 'Hiragino Sans GB', 'Microsoft Yahei', '微软雅黑', Arial, sans-serif; 
     border: 1px solid grey;
     padding: 16px;
+    margin-top: 20px;
 }
 
 /* 我需要一些代码高亮 */
@@ -44,24 +47,29 @@ html {
 
 /* 来一点3D效果 */
 
+html{
+  perspective: 1000px;
+}
 #code {
-    transform: rotate(360deg);
+  position: fixed; 
+  left: 0; 
+  top: 0;
+  -webkit-transition: none;
+  transition: none;
+  -webkit-transform: rotateY(10deg) translateZ(-100px) ;
+          transform: rotateY(10deg) translateZ(-100px) ;
 }
 
-/* 
- * 不整那些花里胡哨的 
 
- * 我来介绍一下我自己
-
- * 首先需要一张白纸
-*/
+/* 不整那些花里胡哨的 */  
 
 #code {
   position: fixed;
   left: 0;
   width: 50%;
-  height: 100%;
 }
+
+/* 首先来一张白纸准备写点东西 */
 `
 
 var resultAdd = `
@@ -69,7 +77,7 @@ var resultAdd = `
   position: fixed;
   right: 0;
   width: 50%;
-  height: 100%;
+  height: 100vh;
   background: #ddd;
   display: flex;
   justify-content: center;
@@ -81,48 +89,95 @@ var resultAdd = `
   background: white;
   width: 100%;
   height: 100%;
+  border-radius: 10px;
 }
 
-/* 接下来把 Markdown 变成 HTML */
-
-/* 接下来给 HTML 加样式 */
+/* 我来介绍一下我自己 */
 `
 
 var md = `
 
 
-## 自我介绍
+## 盛光晟
+----------
 
-我叫盛光晟
-
-1995年2月18日 出生
-东华大学 毕业
-自学前端 半年
-希望应聘前端岗位
+1995年2月出生，毕业于东华大学
+目前自学前端半年，希望应聘前端岗位
 
 ## 技能介绍
+----------
 
 熟悉原生JS、CSS、Vue、Node.JS、AJAX
 
-## 项目介绍
+## 我的链接
+----------
 
-1、 无缝苹果轮播
-2、 太阳光画板
-3、 在线多模块简历
+* ***Github***： https://github.com/sgshy1995
+* ***博客***： https://segmentfault.com/u/nanwuliuxing/articles
+* ***个人网站***： http://106.14.255.91/
 
-## 自我介绍
+## 联系方式
+----------
 
-E-Mail: singlesaul@163.com;
-        singlesaulwork@gmail.com
-Mobile: 17621908460
-Wechat: 17621908460
+#### E-Mail: 
+singlesaul@163.com;
+singlesaulwork@gmail.com
 
+#### Mobile/Wechat: 
+17621908460
+
+### 如果喜欢我的项目，希望联系或关注我
+`
+
+var resultAdd1 = `
+#paper .content{
+  color: #3f3d41;
+}
+
+/* 噢，这是MarkDown格式，让我们来渲染成 HTML (By Marked.js) */
+`
+
+var resultAdd2 = `
+/* 美化一下 HTML 吧 */
+
+#paper .content h2{
+  color: #262821;
+}
+
+#paper .content h3{
+  color: #ddd;
+  background: grey;
+  text-align: center;
+  padding: 5px;
+  width: 80%;
+  margin: 0 auto;
+  border-radius: 5px;
+  transform: scale(1.1);
+}
+
+#paper .content a{
+  text-decoration: none;
+  color: orchid;
+}
+
+#paper .content ul li{
+  list-style: circle;
+  margin-left: 20px;
+}
+
+/* Y(^o^)Y好了，如果你对我的项目感兴趣，一定要Call我哦！ */
 `
 
 writeCode('', result, () => {
   createPaper(() => {
     writeCode(result, resultAdd, () => {
-      writeMarkdown(md)
+      writeMarkdown(md, () => {
+        writeCode(result + resultAdd, resultAdd1, () => {
+          makeToHTML(() => {
+            writeCode(result+resultAdd+resultAdd1,resultAdd2)
+          })
+        })
+      })
     })
   })
 })
@@ -139,7 +194,7 @@ function writeCode(preCode, code, fn) {
       window.clearInterval(id)
       fn.call()
     }
-  }, 10)
+  }, 50)
 }
 
 function createPaper(fn) {
@@ -152,7 +207,7 @@ function createPaper(fn) {
   fn.call()
 }
 
-function writeMarkdown(markdown) {
+function writeMarkdown(markdown,fn) {
   let domContent = document.querySelector('#paper>.content')
   let n = 0
   let id = setInterval(() => {
@@ -161,6 +216,16 @@ function writeMarkdown(markdown) {
     domContent.scrollTop = domContent.scrollHeight
     if (n >= markdown.length) {
       window.clearInterval(id)
+      fn.call()
     }
-  }, 10)
+  }, 50)
+}
+
+function makeToHTML(fn) {
+  var preMDHTML = document.getElementsByClassName('content')
+  var preMD = preMDHTML[0].innerHTML
+  let domContent = document.querySelector('#paper>.content')
+  domContent.innerHTML = marked(preMD)
+  domContent.scrollTop = domContent.scrollHeight
+  fn.call()
 }
